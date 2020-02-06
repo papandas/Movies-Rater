18c4388f1ff301a1338c09a588f5b5e3d029a892
Papan Das <hexoindia@gmail.com>
2020-02-06 20:41:19 +0530

    Serializers

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Movie, Rating
+from .serializers import MovieSerializer, RatingSerializer
 
 # Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    @action(methods=['POST'], detail=True)
+    def rate_movie(self, request, pk=None):
+        response = {'message': 'Its all good'}
+        return Response(response, status=status.HTTP_200_OK)
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
